290c457fb093c1205f5eb868bddb353413d07ffe
wookjinAhn <lexqrt2@pusan.ac.kr>
2022-09-16 02:25:52 +0900

    delete unnecessary codes and modified.

--- depalletizing_mapping_demos/demo/depalletizing_demo.cpp
+++ depalletizing_mapping_demos/demo/depalletizing_demo.cpp
@@ -10,47 +10,6 @@ ros::Publisher pubPointCloud2;
 ros::Publisher pubPolygon;
 tf::TransformBroadcaster* brListener;
 
-void toPCD(std::vector<depalletizing_mapping::Point3D>& data)
-{
-    std::cout << "output : " << data.size() << std::endl;
-    std::string outputPath = "/home/wj/Desktop/Data/polygonize/output/";
-    time_t t;
-    struct tm* timeinfo;
-    time(&t);
-    timeinfo = localtime(&t);
-
-    std::string hour, min;
-
-    if (timeinfo->tm_hour < 10) hour = "0" + std::to_string(timeinfo->tm_hour);
-    else hour = std::to_string(timeinfo->tm_hour);
-
-    if (timeinfo->tm_min < 10) min = "0" + std::to_string(timeinfo->tm_min);
-    else min = std::to_string(timeinfo->tm_min);
-
-    std::string filePath = outputPath + hour + min + ".pcd";
-
-    std::ofstream fout;
-    fout.open(filePath);
-
-    fout << "VERSION" << std::endl;
-    fout << "FIELDS x y z" << std::endl;
-    fout << "SIZE 4 4 4" << std::endl;
-    fout << "TYPE F F F" << std::endl;
-    fout << "COUNT 1 1 1" << std::endl;
-    fout << "WIDTH 1" << std::endl;
-    fout << "HEIGHT " << data.size() << std::endl;
-    fout << "VIEWPOINT 0 0 0 1 0 0 0" << std::endl;
-    fout << "POINTS " << data.size() << std::endl;
-    fout << "DATA ascii" << std::endl;
-
-    for (int i = 0; i < data.size(); i++)
-    {
-        fout << data[i].GetX() << " " << data[i].GetY() << " " << data[i].GetZ() << "\n";
-    }
-
-    fout.close();
-}
-
 void depalletizing_callback(const sensor_msgs::PointCloud2ConstPtr& pointcloud2_msg)
 {
     // for check runtime
@@ -66,8 +25,6 @@ void depalletizing_callback(const sensor_msgs::PointCloud2ConstPtr& pointcloud2_
 
     // get raw data
     dataNode.FromPointCloud2Msgs(*pointcloud2_msg);
-
-    // input에는 없음.
 
     // process data
     treeNode.RunTreeProcess();
